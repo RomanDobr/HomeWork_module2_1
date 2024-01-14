9fbb43bb260655b292c00ee358600d22d9b97858
Roman Dobrokhotov <dobro-ra@yandex.ru>
2024-01-14 21:13:47 +0300

    HomeWork_Module_2_1 v1.2

--- src/homework/homework1/ex1/Runner.java
+++ src/homework/homework1/ex1/Runner.java
@@ -8,18 +8,11 @@ import homework.homework1.ex1.zoo.Zoo;
 public class Runner {
     public static void main(String[] args) {
         Tiger tiger = new Tiger();
-        Zoo<Tiger> tigerZoo = new Zoo<>();
-        tigerZoo.setAnimal(tiger);
-        tigerZoo.getAnimal().growl();
-
         Dog dog = new Dog();
-        Zoo<Dog> dogZoo = new Zoo<>();
-        dogZoo.setAnimal(dog);
-        dogZoo.getAnimal().bark();
-
         Bird bird = new Bird();
-        Zoo<Bird> birdZoo = new Zoo<>();
-        birdZoo.setAnimal(bird);
-        birdZoo.getAnimal().fly();
+        Zoo<Tiger, Bird, Dog> zoo = new Zoo<>(tiger, bird, dog);
+        zoo.getTiger().growl();
+        zoo.getBird().fly();
+        zoo.getDog().bark();
     }
 }
--- src/homework/homework1/ex1/zoo/Zoo.java
+++ src/homework/homework1/ex1/zoo/Zoo.java
@@ -1,19 +1,64 @@
 package homework.homework1.ex1.zoo;
 
-public class Zoo<T extends Animal> {
-    private T animal;
+public class Zoo<T extends Animal, B extends Animal,  D extends Animal> {
+    private T tiger;
+    private B bird;
+    private D dog;
 
-    public void setAnimal(T animal) {
-        System.out.println("Животное " + animal.getClass().getSimpleName() + " добавлено в зоопарк");
-        this.animal = animal;
+    public Zoo(T tiger, B bird, D dog) {
+        this.tiger = tiger;
+        this.bird = bird;
+        this.dog = dog;
     }
 
-    public T getAnimal() {
-        if (animal != null) {
-            T t = animal;
-            animal = null;
-            return t;
+    public void setTiger(T tiger) {
+        checkAnimalNotNull(tiger);
+        this.tiger = tiger;
+        System.out.println(tiger.getClass().getSimpleName() + " добавлен в зоопарк");
+    }
+
+    public void setBird(B bird) {
+        checkAnimalNotNull(bird);
+        this.bird = bird;
+        System.out.println(tiger.getClass().getSimpleName() + " добавлен в зоопарк");
+    }
+
+    public void setDog(D dog) {
+        checkAnimalNotNull(dog);
+        this.dog = dog;
+        System.out.println(tiger.getClass().getSimpleName() + " добавлен в зоопарк");
+    }
+
+    public T getTiger() {
+        checkAnimalOnNull(tiger);
+        T copyT = tiger;
+        tiger = null;
+        return copyT;
+    }
+
+    public B getBird() {
+        checkAnimalOnNull(bird);
+        B copyB = bird;
+        bird = null;
+        return copyB;
+    }
+
+    public D getDog() {
+        checkAnimalOnNull(dog);
+        D copyD = dog;
+        dog = null;
+        return copyD;
+    }
+
+    public void checkAnimalOnNull(Animal animal) {
+        if (animal == null) {
+            throw new RuntimeException("Нет животного в зоопарке");
         }
-        throw new RuntimeException("Нет животного в зоопарке");
+    }
+
+    public void checkAnimalNotNull(Animal animal) {
+        if (animal != null) {
+            throw new RuntimeException("Животное уже в зоопарке");
+        }
     }
 }
